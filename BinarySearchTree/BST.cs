a38c74968ae3b24d44ee61cf149f4fede1ff2065
Madhuri <harshupatil0301@gmail>
2021-07-02 13:22:16 +0530

    [Madhuri]Add : Inserting Elements To The Binary Tree

---
+++ BinarySearchTree/BST.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    class BST<T> where T : IComparable
+    {
+        T NodeData;
+        public BST<T> leftTree { get; set; }
+        public BST<T> rightTree { get; set; }
+        public BST(T NodeData)
+        {
+            this.NodeData = NodeData;
+            leftTree = null;
+            rightTree = null;
+        }
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (leftTree == null)
+                    leftTree = new BST<T>(item);
+                else
+                    leftTree.Insert(item);
+            }
+            else
+            {
+                if (rightTree == null)
+                    rightTree = new BST<T>(item);
+                else
+                    rightTree.Insert(item);
+            }
+        }
+        public void Display()
+        {
+            if (leftTree != null)
+            {
+                leftTree.Display();
+            }
+            Console.WriteLine(NodeData.ToString());
+            if (rightTree != null)
+            {
+                rightTree.Display();
+            }
+        }
+
+    }
+}
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -7,6 +7,14 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To The Binary Search tree Program");
+
+            {
+                BST<int> bst = new BST<int>(56);                  
+                bst.Insert(30);
+                bst.Insert(70);
+                Console.WriteLine("Binary Search Tree -");
+                bst.Display();
+            }
         }
     }
 }
